ae447a13e90b70b56d55a8d11244e039952f5c49
yuki.nagase <nagapon510@gmail.com>
2022-04-10 18:26:33 +0900

    2022/4/10学習分 LINQで書き直し。

--- 標準出力メニュー/【半角スペース区切りの出力】1000個の数値を出力.cs
+++ 標準出力メニュー/【半角スペース区切りの出力】1000個の数値を出力.cs
@@ -3,20 +3,19 @@
 
 using System;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        StringBuilder numbers = new StringBuilder("");
-        for (int i = 1; i <= 1000; i++)
-        {
-            numbers.Append(i);
-            if (i < 1000)
-            {
-                numbers.Append(" ");
-            }
-        }
-        Console.WriteLine(numbers);
+        StringBuilder numbers = new StringBuilder();
+
+        //1～1000の整数をリストにする ⇒ x+1と半角スペースをnumbersに連結 ⇒ Foreachで1000までループ
+        Enumerable.Range(0,1000).ToList().ForEach(x => numbers.Append(x+1).Append(" ")); 
+
+        //numbersを文字列に変換してから引数なしのTrimで末尾の空白を削除
+        Console.WriteLine(numbers.ToString().Trim());
     }
 }
